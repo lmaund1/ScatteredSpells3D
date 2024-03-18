91533a0003f7b4e528a74e6ce98b51476ed5b476
lmaund1 <123125733+lmaund1@users.noreply.github.com>
2024-03-18 00:44:30 +0000

    Added door animations

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{ 
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Debug.Log("hit");
+            animator.SetTrigger("open");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            animator.SetTrigger("close");
+        }
+    }
+}
